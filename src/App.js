df65ae711d32e5d738131fb64281f7add9aad481
Sergey <sergsmile29@gmail.com>
2023-09-14 00:39:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { Product } from './components/Product'
+// План
+// Створити пошук картинок через пексельс
+// Заготовка для запиту, URL, Headers, params:
+// axios.defaults.baseURL = 'https://api.pexels.com/v1/';
+// axios.defaults.headers.common['Authorization'] = API_KEY;
+// axios.defaults.params = {
+//   orientation: 'landscape',
+//   per_page: 15,
+// };
 const App = () => {
-	return (
-		<>
-			<Product />
-		</>
-	)
+	return <></>
 }
 
 export default App
--- src/components/Products.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledMain = styled.main`
-	background-color: #99ffff;
-	padding: 40px 30px 0;
-	max-width: 1280px;
-	margin: 0 auto;
-`
