33e47357bce3577c34cf6c65e66d29f78a21fdf1
Sergey <sergsmile29@gmail.com>
2023-09-15 15:47:12 +0300

    test && || !

---
+++ test.js
@@ -0,0 +1,41 @@
+// console.log(NaN || 2 || undefined || 133)
+
+// console.log(NaN && 2 && undefined)
+
+// console.log(1 && 2 && 3)
+
+// console.log((!1 && 2) || !3)
+
+// console.log(25 || (null && !3))
+
+// console.log(NaN || null || !3 || undefined || 5)
+
+// console.log(NaN || (null && !3 && undefined) || 5)
+
+// console.log((5 === 5 && 3 > 1) || 5)
+
+// const hamburger = 3
+// const fries = 3
+// const cola = 0
+// const nuggets = 2
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+// 	console.log('Done!')
+// }
+
+// let hamburger
+// const fries = NaN
+// const cola = 0
+// const nuggets = 2
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+// 	console.log('Done!')
+// }
+
+// let hamburger
+// const fries = NaN
+// const cola = 0
+// const nuggets = 2
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+// 	console.log('Done!')
+// }
