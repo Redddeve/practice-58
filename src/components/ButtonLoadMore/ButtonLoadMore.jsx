13e65d1b2d1bd483d7c12847588eb54985f72093
Sergey <sergsmile29@gmail.com>
2023-09-19 00:13:43 +0300

    clear

--- src/components/ButtonLoadMore/ButtonLoadMore.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react'
-import { StyledButton } from './ButtonLoadMore.styled'
-import { context } from '../../context/ContextProvider'
-import { setPage } from '../../reducer/action'
-
-export const ButtonLoadMore = ({ handleLoadMore }) => {
-	const { dispatch } = useContext(context)
-	return <StyledButton onClick={() => dispatch(setPage())}>Load More</StyledButton>
-}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { App } from './App'
 import 'modern-normalize'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme'
 import { Global } from './styles/Global'
-import { ContextProvider } from './context/ContextProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<ThemeProvider theme={theme}>
-		<ContextProvider>
-			<App />
-		</ContextProvider>
+		<App />
 		<Global />
 	</ThemeProvider>
 )
