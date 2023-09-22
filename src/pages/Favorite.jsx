55d39973c2c294f1d1e5e3e61fdb0eedd1e0f383
Sergey <sergsmile29@gmail.com>
2023-09-22 15:10:16 +0300

    useSearchParams

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useMyContext } from '../store/context/useMyContext'
 import Modal from '../components/Modal'
 import { removeFav } from '../store/reducer/action'
+import { useSearchParams } from 'react-router-dom'
 
 export const Favorite = () => {
 	const { favorites, toggle, isOpen, dispatch } = useMyContext()
+
 	const handleRemove = item => {
 		dispatch(removeFav(item))
 	}
+
+	const [searchParams, setSearchParams] = useSearchParams()
+	const productName = searchParams.get('name') ?? ''
+
+	const updateQueryString = name => {
+		const nextParams = name ? { name } : {}
+		setSearchParams(nextParams)
+	}
+
+	const visibleProducts = useMemo(
+		() => favorites.filter(product => product.title.toLowerCase().includes(productName.toLowerCase())),
+		[favorites, productName]
+	)
+	const [value, setValue] = useState(1)
 	return (
 		<div>
+			<button onClick={() => setValue(value + 1)}>{value}</button>
+			<div>
+				<input type='text' value={productName} onChange={e => updateQueryString(e.target.value)} />
+			</div>
 			<ul>
-				{favorites?.map(product => (
+				{visibleProducts?.map(product => (
 					<li key={product.id}>
 						<img src={product.thumbnail} alt={product.title} onClick={() => toggle(product)} />
 						<h1>{product.title}</h1>
